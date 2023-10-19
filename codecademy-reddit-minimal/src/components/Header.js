27960c1ed7640b5987be458cd0b6f0cda3c3e93f
AriJai <ajaihooni@gmail.com>
2023-10-18 20:20:04 -0700

    Created prototype header

---
+++ codecademy-reddit-minimal/src/components/Header.js
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+function Header(){
+    return(
+        <header>
+            <h1 className={`${styles.header} ${styles.logo}`}>Reddit<span>Minimal</span></h1>
+            <input className={`${styles.header} ${styles.search}`} placeholder='Search'/>
+            <button className={styles.header} >See Reddit</button>
+        </header>
+    )
+}
+export default Header;
---
+++ codecademy-reddit-minimal/src/components/Subreddit.js
@@ -0,0 +1,12 @@
+import styles from './Subreddit.module.css';
+
+function Subreddit() {
+
+    return(
+        <div>
+            <p>Hi</p>
+        </div>
+    )
+}
+
+export default Subreddit;
