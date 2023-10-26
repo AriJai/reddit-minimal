9cab261bd44d77aadb838513693cebbb5485c8e0
AriJai <ajaihooni@gmail.com>
2023-10-25 21:12:41 -0700

    Working on main page

--- codecademy-reddit-minimal/src/components/ContentListItem.js
+++ codecademy-reddit-minimal/src/components/ContentListItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import styles from '../features/content/Content.module.css';
+import Markdown from 'react-markdown';
 
 function ContentListItem({post}) {
+
+
     return (
         <article key={post.id} className={styles.contentContainer}>
-            <h2 className={styles.title}>{post.title}</h2>
-            <h3>By: {post.author}</h3>
             <h6>{post.subreddit_name_prefixed}</h6>
+            <h5>By: {post.author}</h5>
+            <h2 className={styles.title}>{post.title}</h2>
             <img src={post.url} className={styles.image} alt='' />
-            {post.is_video ? <video controls style={{width:'60%'}}><source src={post.media?.reddit_video.fallback_url}/></video> : ''}
-            <p>{post.selftext}</p>
+            {
+                post.is_video ? 
+                    <video 
+                        controls 
+                        className={styles.image}
+                    >
+                        <source src={post.media?.reddit_video.fallback_url}/>
+                        <source src={post.media?.reddit_video.scrubber_media_url.replace("DASH_96.mp4", "DASH_AUDIO_128.mp4")}/>
+                        <a href={post.media?.reddit_video.scrubber_media_url}></a>
+                    </video>: ''
+            }
+            {post.selftext ? <div className={styles.markdown}><Markdown>{post.selftext}</Markdown></div> : ''}
         </article>
     )
 };
