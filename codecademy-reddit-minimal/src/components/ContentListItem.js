254b7a5d9eb121357f29491f73b1204f5291802a
AriJai <ajaihooni@gmail.com>
2024-09-12 15:53:52 -0700

    Working on gallery images

--- codecademy-reddit-minimal/src/components/ContentListItem.js
+++ codecademy-reddit-minimal/src/components/ContentListItem.js
@@ -2,48 +2,63 @@ import React, { useState } from 'react';
 import styles from '../features/content/Content.module.css';
 import Markdown from 'react-markdown';
 import CommentListItem from './CommentListItem.js';
-import {toggleComments} from '../features/content/contentSlice.js';
+import { toggleComments } from '../features/content/contentSlice.js';
 import { useDispatch, useSelector } from 'react-redux';
 
-function ContentListItem({props, postId, post, onLoadComments, onToggleComments}) {
+function ContentListItem({ props, postId, post, onLoadComments, onToggleComments }) {
     const dispatch = useDispatch();
     const [reply, setReply] = useState(undefined);
 
     const renderComments = () => {
-        if (post.showingComments){
-            return ( 
+        if (post.showingComments) {
+            return (
                 <div className={styles.commentSection}>
                     {post.comments?.map((comments, indexComments) => (
-                            <CommentListItem 
-                                comment={comments} 
-                                key={indexComments} 
-                                postId={postId}
-                                commentId={indexComments}/>
-                        ))}
+                        <CommentListItem
+                            comment={comments}
+                            key={indexComments}
+                            postId={postId}
+                            commentId={indexComments} />
+                    ))}
                 </div>
-        )} else {return null}
+            )
+        } else { return null }
     };
 
     const renderMedia = () => {
-        if( post.is_video) {
+        if (post.is_video) {
             return (
-                <video 
-                        controls 
-                        className={styles.video}
-                    >
-                        <source src={post.media?.reddit_video.fallback_url}/>
-                        <source src={post.media?.reddit_video.scrubber_media_url.replace("DASH_96.mp4", "DASH_AUDIO_128.mp4")}/>
-                        <a href={post.media?.reddit_video.scrubber_media_url}></a>
-                    </video>
+                <video
+                    controls
+                    className={styles.video}
+                >
+                    <source src={post.media?.reddit_video.fallback_url} />
+                    <source src={post.media?.reddit_video.scrubber_media_url.replace("DASH_96.mp4", "DASH_AUDIO_128.mp4")} />
+                    <a href={post.media?.reddit_video.scrubber_media_url}></a>
+                </video>
             )
-        } else if (post.media_metadata) {
-            const listItems = [];
-            for(let i = 0; i < post.gallery_data?.items.length; i++) {listItems.push(i)}
+        } else if (post.media_metadata && post.gallery_data) {
+            const mediaMetadata = post.media_metadata;
+            const images = [];
+            const galleryData = [];
+            for( const item in post.gallery_data.items) {
+                galleryData.push( post.gallery_data.items[item].media_id );
+            }
+            //console.log(galleryData, mediaMetadata[galleryData[0]].p[mediaMetadata[galleryData[0]].p.length-1].u);
+            for( const [key, value] of Object.entries(mediaMetadata)) {
+                if( galleryData.includes(key)) {
+                    console.log(key, value, value.p?.length - 1)
+                    if( value.status === 'valid' ) {
+                        images.push({key: key, value: value.p?.length - 1})
+                    }
+                }
+            }
+            console.log(images);
             return (
                 <div className={styles.gallery}>
-                    {listItems.map((i) => (
-                        <div className={styles.gallery_item}><img key = {i} src={post.media_metadata[post.gallery_data?.items[i].media_id]?.p[3]?.u} className={styles.gallery_img}></img></div>
-                    ))}
+                    {images.map((item, index) => 
+                        <img key = {index} src={post.media_metadata[item.key].p[item.value].u} className={styles.gallery_item}/>
+                    )}
                 </div>
             )
         } else {
@@ -64,19 +79,19 @@ function ContentListItem({props, postId, post, onLoadComments, onToggleComments}
                 renderMedia()
             }
             {
-                post.selftext ? 
-                    <div className={styles.markdown}><Markdown>{post.selftext}</Markdown></div> 
+                post.selftext ?
+                    <div className={styles.markdown}><Markdown>{post.selftext}</Markdown></div>
                     : null
             }
             {
-                post.showingComments? 
-                <button type={'button'} className={styles.commentButton} onClick={() => onToggleComments()}>hide comments</button> :
-                <button type={'button'} className={styles.commentButton} onClick={() => onLoadComments(post.permalink)}>{post.num_comments} comments</button>
+                post.showingComments ?
+                    <button type={'button'} className={styles.commentButton} onClick={() => onToggleComments()}>hide comments</button> :
+                    <button type={'button'} className={styles.commentButton} onClick={() => onLoadComments(post.permalink)}>{post.num_comments} comments</button>
             }
             {
                 renderComments()
             }
-            
+
         </article>
     )
 };
